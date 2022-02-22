aab0ac6af9d5d29152fff7fdb8da728847b8f831
James Wienecke <james.a.wienecke@gmail.com>
2022-02-22 16:45:12 -0600

    Experimenting with different testing libraries.

---
+++ src/main/moviecard/MovieCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+import {Movie} from "../../util/interfaces";
+
+const MovieCard = (props: { movie: Movie; }) => {
+    const movie = props.movie;
+    return (
+        <Container>
+            <h3>Title {movie.title}</h3>
+            <img src={movie.poster} />
+            <h4>Year {movie.year}</h4>
+        </Container>
+    )
+}
+
+export default MovieCard;
