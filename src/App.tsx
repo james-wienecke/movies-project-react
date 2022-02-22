2521b79d3d91bc2ad19f46dd8c9040f2617d6bed
James Wienecke <james.a.wienecke@gmail.com>
2022-02-22 16:52:31 -0600

    Cleaned out App component and wrote a very basic test.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 
-import { collection, getDocs } from 'firebase/firestore';
 import {Container} from "react-bootstrap";
-import NavBar from "./header/NavBar";
-import MovieGrid from "./main/MovieGrid";
-
-import { firestore, mDb } from './util/firebase';
 
 
 function App() {
 
-    const [ movies, setMovies ] = useState([])
-    useEffect(() => {
-        const fetchMovies = async () => {
-            const data = await getDocs(mDb);
-            const docs = data.docs.map(doc => doc.data());
-            console.log(docs);
-        };
-        fetchMovies();
-    }, []);
     return (
         <Container fluid>
-            <header>
-                <NavBar />
-            </header>
-            <main>
-                <MovieGrid movies={movies} />
-            </main>
+
         </Container>
     );
 }
