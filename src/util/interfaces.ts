fae83f3c038878105f9ccac0be37e89d5291e1d5
James Wienecke <james.a.wienecke@gmail.com>
2022-02-22 23:21:16 -0600

    Created initial definition of the Movie interface.

---
+++ src/util/interfaces.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+    title: string,
+    director: string,
+    plot: string,
+    poster: string,
+    year: number,
+    rating: number,
+    actors: string[],
+    genres: string[]
+}
